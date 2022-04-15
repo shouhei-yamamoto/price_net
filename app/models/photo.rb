f39aa7f5eedb7a83c1941337017fd9b103f1142f
shouhei-yamamoto <elefish19920509@gmail.com>
2022-04-15 15:41:38 +0900

    geocoderによる地図検索機能の追加

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,9 @@
 class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :image, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   # validates :price, presence: true
 end
