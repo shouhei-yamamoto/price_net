0eaec6de9f62023381903c53da42ec31fba6e2d6
shouhei-yamamoto <elefish19920509@gmail.com>
2022-04-26 11:57:46 +0900

    デザイン等修正

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe Comment, type: :model do
   describe 'コメント投稿テスト', type: :model do
     context '空で登録しようとする時' do
       it 'バリデーションにひっかかる' do
-        # @user = FactoryBot.create(:user)
-        # @photo = FactoryBot.create(:photo, user: @user)
-        # user = User.new(name: 'test', email: 'test@address', password: 'testdayo', password_confirmation: 'testdayo')
         comment = Comment.new(content: '')
-        # comment = Comment.new(content: '', name: @user.name)
-        # comment = Comment.new(content: '', photo_id: @photo.id, user_id: @user.id)
         expect(comment).not_to be_valid
       end
     end
